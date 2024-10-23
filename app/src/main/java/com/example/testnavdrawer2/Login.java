438bc041d6a0cdc868d1b98d106c0cd8c023cffd
LinkAnJarad <linkanjarad@yahoo.com>
2024-10-23 12:21:06 +0800

    Added login and connection to database

--- app/src/main/java/com/example/testnavdrawer2/Login.java
+++ app/src/main/java/com/example/testnavdrawer2/Login.java
@@ -1,5 +1,6 @@
 package com.example.testnavdrawer2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -12,11 +13,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     private Button login_btn;
-
-
+    private TextInputEditText tf_email, tf_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +53,87 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         });
 
         login_btn = (Button) findViewById(R.id.login_btn);
+        tf_email = (TextInputEditText) findViewById(R.id.tf_email);
+        tf_password = (TextInputEditText) findViewById(R.id.tf_password);
 
         login_btn.setOnClickListener(this);
+    }
+
+    private void loginUser() {
+
+        handleSSLHandshake();
+
+        String email = tf_email.getText().toString().trim();
+        String password = tf_password.getText().toString().trim();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://192.168.254.118/login.php";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(Login.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Login.this, error + "", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                // Send email and password as POST parameters
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+
+        // Add the request to the RequestQueue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @SuppressLint("TrulyRandom")
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
     }
 
     @Override
     public void onClick(View view) {
 
         if (view.getId() == R.id.login_btn) {
-            //Toast.makeText(this, "Login Pressed!", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(Login.this, MainActivity.class);
-            startActivity(intent);
+            loginUser();
+            //Intent intent = new Intent(Login.this, MainActivity.class);
+            //startActivity(intent);
         }
     }
 }
