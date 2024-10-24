31459957742429cf76070884eb5107893bfb99c1
LinkAnJarad <linkanjarad@yahoo.com>
2024-10-24 19:21:43 +0800

    Add sign up page part 3 (students) - 4

--- app/src/main/java/com/example/testnavdrawer2/ui/qr/QRFragment.java
+++ app/src/main/java/com/example/testnavdrawer2/ui/qr/QRFragment.java
@@ -1,20 +1,32 @@
 package com.example.testnavdrawer2.ui.qr;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.testnavdrawer2.R;
 import com.example.testnavdrawer2.databinding.FragmentQrBinding;
+import com.google.android.material.button.MaterialButton;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class QRFragment extends Fragment {
 
     private FragmentQrBinding binding;
+
+    private ImageView qrCodeImageView;
+    private MaterialButton btn_generate_qr;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +36,38 @@ public class QRFragment extends Fragment {
         binding = FragmentQrBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        qrCodeImageView = (ImageView) root.findViewById(R.id.qrCodeImageView);
+        btn_generate_qr = (MaterialButton) root.findViewById(R.id.btn_generate_qr);
+
+
+        btn_generate_qr.setOnClickListener(v -> generateQRCode());
+
         return root;
+
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+
+    //Generate ng QRCode
+    private void generateQRCode() {
+
+
+        String qrContent = "Test content";
+
+        try {
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            BitMatrix bitMatrix = barcodeEncoder.encode(qrContent, BarcodeFormat.QR_CODE, 400, 400);
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            qrCodeImageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Error generating QR code", Toast.LENGTH_SHORT).show();
+        }
     }
 }
