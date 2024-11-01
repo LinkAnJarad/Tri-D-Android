f645575324a3c77d9e13d49c51f8c86c63563739
LinkAnJarad <linkanjarad@yahoo.com>
2024-11-01 12:02:58 +0800

    🐮add initial admin home ui🐮

--- app/src/main/java/com/example/testnavdrawer2/AdminActivity.java
+++ app/src/main/java/com/example/testnavdrawer2/AdminActivity.java
@@ -30,20 +30,13 @@ public class AdminActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarAdmin.toolbar);
-        binding.appBarAdmin.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .setAnchorView(R.id.fab).show();
-            }
-        });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_homeadmin, R.id.nav_qradmin, R.id.nav_admin_history, R.id.nav_admin_logs, R.id.nav_logout)
+                R.id.nav_adminhome, R.id.nav_qradmin, R.id.nav_admin_history, R.id.nav_admin_logs, R.id.nav_logout)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_admin);
